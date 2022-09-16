c7fc6f6486383fae46754433ba1ccc827bcb9882
Mirko Brüggemann <mirko_brueggemann@gmx.de>
2022-09-16 14:55:02 +0200

    add subsetting test

--- tests/testthat/test_subsetting.R
+++ tests/testthat/test_subsetting.R
@@ -3,9 +3,15 @@ test_that("Binding site merging works", {
     files <- system.file("extdata", package="BindingSiteFinder")
     load(list.files(files, pattern = ".rda$", full.names = TRUE))
 
+    # test range of subset 1
+    rng1 = getRanges(bds)[1:100]
+    rng2 = getRanges(bds[1:100])
+    expect_identical(rng1, rng2)
 
-    a
-
-
-
+    # test range of subset 2
+    set.seed(1234)
+    samp = sample(c(1:length(getRanges(bds))), 10)
+    rng1 = getRanges(bds)[samp]
+    rng2 = getRanges(bds[samp])
+    expect_identical(rng1, rng2)
 })
