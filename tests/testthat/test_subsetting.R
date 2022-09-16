59c895c89b814e18f7d0f60e109c51311bc35bb5
Mirko Brüggemann <mirko_brueggemann@gmx.de>
2022-09-16 14:54:16 +0200

    fix sorting

---
+++ tests/testthat/test_subsetting.R
@@ -0,0 +1,11 @@
+test_that("Binding site merging works", {
+    # load data
+    files <- system.file("extdata", package="BindingSiteFinder")
+    load(list.files(files, pattern = ".rda$", full.names = TRUE))
+
+
+    a
+
+
+
+})
